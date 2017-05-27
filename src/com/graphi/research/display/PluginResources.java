ce2392dabf221beb1089c2785ec331c137ba0e0f
denkers <kyle.business@inbox.com>
2017-05-27 21:32:13 +1200

    display.PluginResources

---
+++ src/com/graphi/research/display/PluginResources.java
@@ -0,0 +1,15 @@
+//======================================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi-research-plugin
+//======================================================
+
+package com.graphi.research.display;
+
+import com.graphi.display.layout.util.ResourceMap;
+
+public class PluginResources extends ResourceMap
+{
+    @Override
+    protected void initResources() {}
+}
