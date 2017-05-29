b6e1e78aa8c4bfcae8007457d87cc2a07e3286f2
denkers <kyle.business@inbox.com>
2017-05-29 16:09:19 +1200

    Added seeding methods and seeding transformations to network.NetworkSeeder

--- src/com/graphi/network/NetworkSeeder.java
+++ src/com/graphi/network/NetworkSeeder.java
@@ -6,8 +6,15 @@
 
 package com.graphi.network;
 
+import com.graphi.graph.Edge;
 import com.graphi.graph.Node;
+import edu.uci.ics.jung.graph.Graph;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class NetworkSeeder 
@@ -40,6 +47,83 @@ public class NetworkSeeder
         authenticPercent        =   0.0;
     }
     
+    public void generateSeeds(Map<Integer, Node> nodes, Graph<Node, Edge> graph)
+    {
+        int seedSize    =   (int) (nodes.size() * seedPercent);
+        
+        if(seedMethod == RANDOM_FRIEND_SEED) 
+            seedSize     =   seedSize / 2;
+        
+        if(seedMethod == RANDOM_NODE_SEED)
+            randomNodeMethod(nodes, seedSize);
+
+        else if(seedMethod == RANDOM_FRIEND_SEED)
+            addRandomNeighbour(nodes, graph, seedSize);
+    }
+    
+    public void randomNodeMethod(Map<Integer, Node> nodes, int seedSize)
+    {
+        List<Integer> nodesCopy =   new LinkedList<>(nodes.keySet());
+        Collections.shuffle(nodesCopy);
+        
+        for(int i = 0; i < seedSize; i++)
+            seeds.add(nodes.get(nodesCopy.get(i)));
+    }
+    
+    public void addRandomNeighbour(Map<Integer, Node> nodes, Graph<Node, Edge> graph, int seedSize)
+    {
+        List<Integer> nodesCopy =   new LinkedList<>(nodes.keySet());
+        int expectedSeedSize    =   seedSize * 2;
+        Collections.shuffle(nodesCopy);
+        
+        for(int i = 0; i < nodes.size() && seeds.size() < expectedSeedSize; i++)
+        {
+            Node randomNode          =   nodes.get(nodesCopy.get(i));
+            List<Node> neighbours    =   new ArrayList<>(graph.getNeighbors(randomNode));
+            Collections.shuffle(neighbours);
+            
+            if(!seeds.contains(randomNode))
+            {
+                seeds.add(randomNode);
+            
+                for(int j = 0; j < neighbours.size(); j++)
+                {
+                    Node randomNeighbour    =   neighbours.get(j);
+                    if(!seeds.contains(randomNeighbour))
+                    {
+                        seeds.add(randomNeighbour);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    
+    public void runSeedInfluenceTransform()
+    {
+        for(Node seed : seeds)
+        {
+            InfluenceAgent agentSeed    =   (InfluenceAgent) seed;
+            agentSeed.setInfluenced(true);
+        }
+    }
+    
+    public void runSeedAuthenticityTransform()
+    {
+        if(authenticityMode)
+        {
+            double falsePercent     =   1.0 - authenticPercent;
+            int falseSize           =   (int) (seeds.size() * falsePercent);
+            List<Node> seedsCopy    =   new ArrayList<>(seeds);
+            Collections.shuffle(seedsCopy);
+            
+            for(int i = 0; i < falseSize; i++)
+            {
+                InfluenceAgent agentSeed    =   (InfluenceAgent) seedsCopy.get(i);
+                agentSeed.setAuthentic(false);
+            }
+        }
+    }
     
     public void enableAuthenticityMode(double authenticPercent)
     {
@@ -50,6 +134,11 @@ public class NetworkSeeder
     public void disableAuthenticityMode()
     {
         authenticityMode    =   false;
+    }
+    
+    public void resetSeeds()
+    {
+        seeds.clear();
     }
 
     public Set<Node> getSeeds()
